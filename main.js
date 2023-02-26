b3504ffb5e5b14b10e63cb5715b03a5848131cb6
arifditya30 <aryfadtya@gmail.com>
2023-02-26 23:27:59 +0700

    Add contact

--- main.js
+++ main.js
@@ -26,11 +26,38 @@ function toogleDetail(e){
 
     let item = $(target).parents(".about-exp-item")
     let detail = $(item).children(".about-exp-item-detail")
+    console.log(detail)
     $(detail).slideToggle()
 
    
 
     }
+
+function onFormSubmit(e){
+    e.preventDefault()
+
+    let email = ($("#inp_email"))
+    let subject = ($("#inp_subject"))
+    let message = ($("#inp_message"))
+
+    if(!$(email).val()){
+        return alert("Email is Required")
+    } else  if(!$(subject).val()){
+        return alert("Subject is Required")
+    } if(!$(message).val()){
+        return alert("Message is Required")
+    }else{
+         alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+
+    }
+    
+
+
+    // console.log($(subject).val())
+}
  
     
     
